34eb1addb8426f54320ca119c33e2f4f59b7f26a
matthewdiorio <37198911+matthewdiorio@users.noreply.github.com>
2023-02-23 16:41:50 -0500

    Initial Commit

---
+++ src/router/index.js
@@ -0,0 +1,6 @@
+import {createRouter, createWebHistory } from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL)
+})
